2c6c4ae284751e3a835c93a1ee94a5c51163f5d0
sharad-99 <33867234+sharad-99@users.noreply.github.com>
2020-06-10 23:05:21 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,60 @@
+import numpy as np
+from PIL import Image
+import cv2
+
+from help_functions import *
+
+
+#Pre processing tasks
+def my_PreProc(data):
+    assert(len(data.shape)==4)
+    assert (data.shape[1]==3) 
+    train_imgs = rgb2gray(data)
+    train_imgs = dataset_normalized(train_imgs)
+    train_imgs = clahe_equalized(train_imgs)
+    train_imgs = adjust_gamma(train_imgs, 1.2)
+    train_imgs = train_imgs/255.  #reduce to 0-1 range
+    return train_imgs
+
+
+
+
+#CLAHE 
+def clahe_equalized(imgs):
+    assert (len(imgs.shape)==4)  
+    assert (imgs.shape[1]==1) 
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    imgs_equalized = np.empty(imgs.shape)
+    for i in range(imgs.shape[0]):
+        imgs_equalized[i,0] = clahe.apply(np.array(imgs[i,0], dtype = np.uint8))
+    return imgs_equalized
+
+
+#Normalization
+def dataset_normalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[1]==1)  #check the channel is 1
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized
+
+#Area selection using BB
+def area_select(test_array,h_max=255):
+    input_array = np.copy(test_array) 
+    BB = sp.ndimage.generate_binary_structure(2,2).astype(np.int)
+    inside_mask = sp.ndimage.binary_erosion(~np.isnan(input_array), structure=BB)
+    output_array = np.copy(input_array)
+    output_array[inside_mask]=h_max
+    output_old_array = np.copy(input_array)
+    output_old_array.fill(0)   
+    BB = sp.ndimage.generate_binary_structure(2,1).astype(np.int)
+    while not np.array_equal(output_old_array, output_array):
+        output_old_array = np.copy(output_array)
+        output_array = np.maximum(input_array,sp.ndimage.grey_erosion(output_array, size=(3,3), footprint=BB))
+    return output_array
+
+x = plt.imread("test.jpg")
